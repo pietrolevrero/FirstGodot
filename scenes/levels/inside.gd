3c6a85d913fd66b6c97e577352d780a63af7e865
Pietro Levero <levrero.pietro@gmail.com>
2024-03-27 11:04:54 +0000

    started ui

--- scenes/levels/inside.gd
+++ scenes/levels/inside.gd
@@ -1,6 +1,8 @@
 extends LevelParent
 
+var outisde_level_scene: PackedScene = preload("res://scenes/levels/outside.tscn")
 
 func _on_exit_gate_area_body_entered(_body):
 	var tween = create_tween()
 	tween.tween_property($Player, "speed", 0, 0.3)
+	get_tree().change_scene_to_packed(outisde_level_scene)
--- scenes/levels/outside.gd
+++ scenes/levels/outside.gd
@@ -4,3 +4,4 @@ extends LevelParent
 func _on_gate_player_entered_gate(_body):
 	var tween = create_tween()
 	tween.tween_property($Player, "speed", 0, 0.3)
+	get_tree().change_scene_to_file("res://scenes/levels/inside.tscn")
